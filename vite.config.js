9b477165ad00633df6006d6769c16c4f920a5425
RevX Deploy <deploy@revx.com>
2026-04-21 08:27:53 +0530

    Add GitHub actions deploy for Vite

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // This ensures paths render correctly when hosted in a sub-folder like GitHub pages
+  base: '/baketales/'
+})
